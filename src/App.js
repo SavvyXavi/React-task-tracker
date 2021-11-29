522cf769beaecd2d37937db49e3382ba7b5daf44
Xavi-Dev <xjohndev@outlook.com>
2021-11-29 18:06:27 -0500

    New task can be submitted to list of tasks

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ function App() {
     }
 ])
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = {id, ...task}
+    setTasks([...tasks,  newTask])
+  }
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
@@ -33,7 +39,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
       <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
       ) : ('No Tasks to Show')}
